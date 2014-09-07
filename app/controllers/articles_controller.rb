0a72e9b4f636d5997fc7130cf25cabca18c32eef
Gladkikh Prohor <prohor33@gmail.com>
2014-09-07 23:28:14 +0400

    fix local link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,15 @@ class ArticlesController < ApplicationController
   def new
     # render plain: "how are you?"
     
-    for i in 0..5
-      puts "#{i}"
-    end
-    puts "run myrender"
+    # for i in 0..5
+      # puts "#{i}"
+    # end
+    # puts "run myrender"
     # myrender("test")
     
     @articles = Article.all
+    
+    # redirect_to "articles/chat"
   end
  
   def create
@@ -19,7 +21,17 @@ class ArticlesController < ApplicationController
     
     @article.save
     # redirect_to @article
-    redirect_to "http://localhost:3000/articles/new"
+    # redirect_to "http://localhost:3000/articles/new"
+    # @articles = Article.all
+    
+    # render :template => "articles/new"
+    # redirect_to "/articles/new"
+    
+    redirect_to "/articles/new"
+  end
+  
+  def chat
+    @articles = Article.all
   end
   
   def myrender(render_text)
